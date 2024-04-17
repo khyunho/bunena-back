045624ebcf787025a51add2bebd64a4649e38e01
khyunho <smacaron@naver.com>
2024-04-17 14:51:05 +0900

    Initial commit

---
+++ src/main/java/com/banana/bunena/BunenaApplication.java
@@ -0,0 +1,13 @@
+package com.banana.bunena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banana/bunena/BunenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banana.bunena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
